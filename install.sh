c2a5bd93a86d44be566832a899c800944b0d8a83
gibsonxavier <gibson.xavier@arago.de>
2019-01-08 16:03:56 +0100

    modified:   install.sh 	modified:   setup.cfg 	modified:   setup.py 	deleted:    dist/put_mars_nodes-0.0.1-py3-none-any.whl 	modified:   put_mars_nodes.egg-info/PKG-INFO 	modified:   put_mars_nodes.egg-info/SOURCES.txt 	modified:   put_mars_nodes.egg-info/dependency_links.txt  Untracked files: 	build/ 	dist/put_mars_nodes-1.0.0.tar.bz2 	dist/put_mars_nodes-1.0.0.tar.gz

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ if [ -d "$DIRECTORY" ]; then
   mv $CURRENT_DIRECTORY/graphit.conf $DEFAULT_DIRECTORY
   mv $CURRENT_DIRECTORY/put_mars_node.py $DEFAULT_DIRECTORY
   mv $CURRENT_DIRECTORY/helper.py $DEFAULT_DIRECTORY
+  chmod -R 755 $DEFAULT_DIRECTORY/put_mars_node.py
+  chmod -R 755 $DEFAULT_DIRECTORY/helper.py
+  chmod -R 644 $DEFAULT_DIRECTORY/graphit.conf
 fi
 
 if [  -f $DEFAULT_DIRECTORY/put_mars_node.py ]; then
