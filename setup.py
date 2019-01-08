f5cba4298596339bc51a6f35c29d6c194b1ac6b6
gibsonxavier <gibson.xavier@arago.de>
2019-01-08 16:26:45 +0100

    modified:   MANIFEST.in 	deleted:    dist/put_mars_nodes-0.0.1-py3-none-any.whl 	modified:   put_mars_nodes.egg-info/PKG-INFO 	modified:   put_mars_nodes.egg-info/SOURCES.txt 	modified:   put_mars_nodes.egg-info/dependency_links.txt 	modified:   put_mars_nodes.egg-info/requires.txt 	modified:   setup.cfg 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     url="https://github.com/gibsonxavier/mars_importer",
     packages=setuptools.find_packages(),
     install_requires=[
-        'graphit-tool>=0.7',
+        'graphit-tool>=0.0',
 		'gevent>=1.3.7',
 		'docopt>=0.6.2',
 		
